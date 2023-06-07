903a88d73b1e947bcb654d1bf966054e682ac253
mr7aali <mr7aali@gmail.com>
2023-06-07 12:48:02 +0600

    setup husky lint prettier

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-const app:Application = express();
-
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-
-export default app;
+export default app
